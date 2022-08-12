0682401c301f8b78baf54653e5d7dd703f3b3dd0
chimexGitHub <okekechimezieglory@gmail.com>
2022-08-12 13:13:42 +0100

    wrote the DepartmentOption model,service, interface and controller for the DepartmentOption API

---
+++ APIs/Controllers/DepartmentOptionController.cs
@@ -0,0 +1,55 @@
+﻿using BusinessLayer.Interface;
+using DataLayer.Dtos;
+using DataLayer.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIs.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentOptionController : ControllerBase
+    {
+        private readonly IDepartmentOptionService _service;
+        private readonly FPNOOCRContext _context;
+
+        public DepartmentOptionController(IDepartmentOptionService service, FPNOOCRContext context)
+        {
+            _service = service;
+            _context = context;
+        }
+
+        [HttpPost("[action]")]
+        public async Task<ResponseModel> AddDepartmentOption(DepartmentOptionDto model) => await _service.AddDepartmentOption(model);
+
+        // [HttpGet("[action]")]
+        //public async Task<IEnumerable<DepartmentOptionDto>> GetDepartmentOptions()
+        //{
+        //    return await _context.DEPARTMENT_OPTION.Where(a => a.Active).Include(f => f.Name)
+        //        .Select(f => new DepartmentOptionDto
+        //        {
+        //            Name = f.Name,
+        //            DepartmentId = (long)f.DepartmentId,
+        //            Id = f.Id,
+        //            Active = f.Active,
+        //        })
+        //        .OrderBy(a => a.Name)
+        //        .ToListAsync();
+        //}
+
+       // [HttpGet("{id}")]
+       // public DepartmentOption GetById(long id) => _service.GetById(id);
+
+        [HttpPost]
+        public async Task<ResponseModel> UpdateDepartmentOption(DepartmentOptionDto model) => await _service.UpdateDepartmentOption(model);
+
+        [HttpPost("[action]")]
+        public async Task<ResponseModel> DeleteDepartmentOption(long id) => await _service.DeleteDepartmentOption(id);
+
+    }
+}
---
+++ BusinessLayer/Interface/IDepartmentOptionService.cs
@@ -0,0 +1,17 @@
+﻿using DataLayer.Dtos;
+using DataLayer.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Interface
+{
+    public interface IDepartmentOptionService: IRepository<DepartmentOption>
+    {
+        Task<ResponseModel> AddDepartmentOption(DepartmentOptionDto model);
+        Task<ResponseModel> UpdateDepartmentOption(DepartmentOptionDto model);
+        Task<ResponseModel> DeleteDepartmentOption(long id);
+        //Task<ResponseModel> GetDepartmentOptionsByDepartmentId(string departmentId, bool isAdmin);
+    }
+}
